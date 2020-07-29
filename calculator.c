51031a18b39405fb8b646541660833e4d03ac80b
ammar004 <68864229+ammar004@users.noreply.github.com>
2020-07-29 16:02:48 +0530

    Add files via upload

---
+++ calculator.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int a = 4, b = 5, c;
+    c=a+b;
+    printf("sum = %d, \n", c);
+    c=a-b;
+    printf("difference = %d, \n", c);
+    c=a*b;
+    printf("product = %d, \n", c);
+    c=a/b;
+    printf("division= %d, \n", c);
+    c=a%b;
+    printf("remaimder when a divided by b = %d, \n", c);
+
+
+
+
+}
